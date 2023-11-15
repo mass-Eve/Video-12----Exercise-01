d7a0b2ceb180b255f2c0ee57133780f3065b7641
mass-Eve <detoxedankur@gmail.com>
2023-11-15 23:38:33 +0530

    ---

---
+++ 02-basic-arithmetic-operators.py
@@ -0,0 +1,22 @@
+num1 = 5
+num2 = 6
+
+print("Addition of num1 and num2 is: ", (num1 + num2))
+
+print("Subtraction of num1 from num2 is: ", (num1 - num2))
+print("Subtraction of num2 from num1 is: ", (num2 - num1))
+
+print("Multiplication of num1 and num2 is: ", (num1 * num2))
+
+print("Quotient obtained on dividing num1 by num2 is: ", (num1 / num2))
+print("Quotient obtained on dividing num2 by num1 is: ", (num2 / num1))
+
+print("Remainder obtained on dividing num1 by num2 is: ", (num1 % num2))
+print("Remainder obtained on dividing num2 by num1 is: ", (num2 % num1))
+
+print("Num1 raised to Num2 is: ", num1**num2)
+print("Num2 raised to Num1 is: ", num1**num2)
+
+print("Floor division of num1//num2 is: ", num1//num2)
+print("Floor division of num2//num1 is: ", num2//num1)
+
---
+++ 03-type-of-data.py
@@ -0,0 +1,14 @@
+# Print the type of data
+
+number = 5
+decimal = 5.3
+character = 'ankur'
+bool_True = True
+bool_False = False
+
+print('The type of data in number variable is: ',type(number))
+print('The type of data in decimal variable is: ',type(decimal))
+print('The type of data in character variable is: ',type(character))
+print('The type of data in bool_true variable is:',type(bool_True))
+print('The type of data in bool_false variable is:',type(bool_False))
+
