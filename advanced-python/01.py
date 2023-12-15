d19fe9e3b4cd90b2464fd716dd7a3500c3d28c62
mass-eve <detoxedankur@gmail.com>
2023-12-15 20:05:30 +0530

    -

---
+++ advanced-python/01.py
@@ -0,0 +1,5 @@
+# will give an error
+a = 10, b= 10
+
+# No error
+a = 10; b = 'ap'    # a semi-colon create a logical break
---
+++ advanced-python/02.py
@@ -0,0 +1,4 @@
+a = 1 + 2 + 3+\
+5 + 6 + 4
+
+# it converts two break lines into one logical line
