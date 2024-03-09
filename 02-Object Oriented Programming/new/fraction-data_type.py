51ac24334c9571f27b477b3a1a47da347f860f5f
mass-eve <detoxedankur@gmail.com>
2024-03-09 21:32:04 +0530

    functionality for adding multiple fractions done

--- 02-Object Oriented Programming/new/fraction-data_type.py
+++ 02-Object Oriented Programming/new/fraction-data_type.py
@@ -3,41 +3,63 @@ class Fraction:
         self.nr = n
         self.dr = d
 
+    # for printing 
     def __str__(self):
         return f"{self.nr}/{self.dr}"
 
-    def __add__(self, other):
-        result_nr = self.nr * other.dr + self.dr * other.nr
-        result_dr = self.dr * other.dr
+    # to add two fractions 
+    def __add__(self, *other):
 
-        return f"{result_nr}/{result_dr}"
+        try:
+            result_nr = self.nr
+            result_dr = self.dr
 
-    def __sub__(self, other):
+            # iterate through every tuple of fraction numbers
+            for fraction in other:
+                result_nr = (result_nr * fraction.dr) + (fraction.nr * result_dr)
+                result_dr = result_dr * fraction.dr
+
+            return Fraction(result_nr, result_dr)
+
+        except Exception as e:
+            print(e)
+
+    # to subtract two fractions
+    def __sub__(self, *other):
         result_nr = self.nr * other.dr - self.dr * other.nr
         result_dr = self.dr * other.dr
-
         return f"{result_nr}/{result_dr}"
 
-    def __mul__(self, other):
+    # to multiply two fractions
+    def __mul__(self, *other):
         result_nr = self.nr * other.nr
         result_dr = self.dr * other.dr
-
         return f"{result_nr}/{result_dr}"
 
-    def __truediv__(self, other):
+    # to divide two fractions
+    def __truediv__(self, *other):
         result_nr = self.nr * other.dr
         result_dr = self.dr * other.nr
-
         return f"{result_nr}/{result_dr}"
 
 
-x = Fraction(4, 5)
+x = Fraction(2, 3)
 print(x)
 
-y = Fraction(5, 5)
+y = Fraction(4, 5)
 print(x)
 
-print(x + y)
-print(x - y)
-print(x * y)
-print(x / y)
+# print(x + y)
+# print(x - y)
+# print(x * y)
+# print(x / y)
+
+a = Fraction(6, 7)
+print(a)
+
+b = Fraction(8, 9)
+print(b)
+
+# print(x * y * a * b )
+
+print(x + y + a + b)
