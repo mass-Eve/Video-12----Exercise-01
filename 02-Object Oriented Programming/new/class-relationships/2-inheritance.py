8af45a3cacbccd7ee03b765710715994e43bbfe2
mass-eve <detoxedankur@gmail.com>
2024-03-10 21:58:57 +0530

    basics of inheritance in python

---
+++ 02-Object Oriented Programming/new/class-relationships/2-inheritance.py
@@ -0,0 +1,25 @@
+class User:
+
+    def login():
+        pass
+
+    def register():
+        pass
+
+# Student Class is inheriting from the User Class
+class Student(User):
+
+    def enroll():
+        pass
+
+    def review():
+        pass
+
+# Instructor Class is inheriting from the User Class
+class Instructor(User):
+
+    def enroll():
+        pass
+
+    def answerQuestions():
+        pass
