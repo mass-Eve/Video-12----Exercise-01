6907ec0480179b807194b253059e96eadb290a75
mass-eve <detoxedankur@gmail.com>
2024-03-11 23:18:07 +0530

    created menu

--- 02-Object Oriented Programming/new/library-management-system-project/student-class.py
+++ 02-Object Oriented Programming/new/library-management-system-project/student-class.py
@@ -32,17 +32,38 @@ class StudentID:
 
     def updateDetails(self):
 
-        # menu
-        def menu(self):
-            print("WHAT WOULD YOU LIKE TO UPDATE ?")
-            print("1. ENTER 1 TO UPDATE THE NAME OF THE STUDENT ")
-            print("2. ENTER 2 TO UPDATE THE KAKSHA OF THE STUDENT")
-            print("3. ENTER 3 TO UPDATE THE SECTION OF THE STUDENT")
-            print("4. ENTER 4 TO UPDATE THE ROLL NUMBER OF THE STUDENT")
-            print("5. ENTER 5 TO UPDATE THE GENDER OF THE STUDENT")
-            print("6. ENTER 6 TO UPDATE THE HOUSE ADDRESS OF THE STUDENT")
+        while(True):
+            # menu
+            def menu(self):
+                print("WHAT WOULD YOU LIKE TO UPDATE ?")
+                print("1. ENTER 1 TO UPDATE THE NAME OF THE STUDENT ")
+                print("2. ENTER 2 TO UPDATE THE KAKSHA OF THE STUDENT")
+                print("3. ENTER 3 TO UPDATE THE SECTION OF THE STUDENT")
+                print("4. ENTER 4 TO UPDATE THE ROLL NUMBER OF THE STUDENT")
+                print("5. ENTER 5 TO UPDATE THE GENDER OF THE STUDENT")
+                print("6. ENTER 6 TO UPDATE THE HOUSE ADDRESS OF THE STUDENT")
 
-            choice = int(input("~ "))
+                choice = int(input("~ "))
+
+                match choice:
+                    case 1:
+                        updateName()
+                    case 2:
+                        updateKaksha()
+                        self.assignStudentID()
+                    case 3:
+                        updateSection()
+                        self.assignStudentID()
+                    case 4:
+                        updateRoll()
+                        self.assignStudentID()
+                    case 5:
+                        updateGender()
+                        self.assignStudentID()
+                    case 6:
+                        updateAdd()
+                    case _:
+                        print("Check your inputs and try again later")
 
         def updateName(self):
             newName = input("Enter the new name of the student : ")
