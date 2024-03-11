a311ba0928211a24b8e9c63c234579ee1fd4bce2
mass-eve <detoxedankur@gmail.com>
2024-03-11 22:16:48 +0530

    functionality to update a book's details created

--- 02-Object Oriented Programming/new/library-management-system-project.py
+++ 02-Object Oriented Programming/new/library-management-system-project.py
@@ -21,9 +21,6 @@ class Book:
         self.total_copies = total_number_of_copies
         self.quantities_available = available_copies_amount
 
-    def updateBookDetails(self):
-        pass
-
     def displayBookDetails(self):
         print(f"BOOK NAME : {self.book_name}")
         print(f"AUTHOR NAME : {self.author}")
@@ -34,6 +31,54 @@ class Book:
         print(f"PRICE : {self.selling_price}")
         print(f"TOTAL COPIES AVAILABLE IN LIBRARY : {self.total_copies}")
         print(f"TOTAL COPIES AVAIABLE TO BORROW : {self.quantities_available}")
+
+    def updateBookDetails(self):
+        
+        # update book name
+        def updateName(self):
+            newTitle = input("Enter the new title of the book : ")
+            self.book_name = newTitle
+            print("Book Title Updated Successfully........")
+
+        # update author name
+        def updateAuthor(self):
+            newName = input("Enter the new author name : ")
+            self.author = newName
+
+        # update publication house name
+        def updatePublication(self):
+            newPub = input("Enter the new publication name : ")
+            self.publication = newPub
+
+        # update genre of the book
+        def updateGenre(self):
+            newGenre = input("Enter the updated value for genre : ")
+            self.genre = newGenre
+
+        # update the publishing year
+        def updateYear(self):
+            newYear = int(input("Enter the year of publishing : "))
+            self.pub_year = newYear
+
+        # update the ISBN Code of the book
+        def updateISBN(self):
+            newISBN = int(input("Enter the correct value of ISBN : "))
+            SELF.isbn_number = newISBN
+
+        # update the selling price of the book
+        def updatePrice(self):
+            newPrice = int(input("Enter the new price of the book : "))
+            self.price = newPrice
+
+        # update total number of copies of the book
+        def updateTotalNoOfCopies(self):
+            total = int(input("Enter the updated number of total copies of this book : "))
+            self.total_copies = total
+
+        # update total number of copies of the book available for the people to borrow
+        def updateNoOfCopiesAvailableToBorrow(self):
+            total_available = int(input("Enter the total number of copies available to borrow : "))
+            self.quantities_available = total_available
 
     def checkAvaiablility(self):
         if (quantities_available > total_copies):
